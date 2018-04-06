596d5516bc8c0116248712ce2782e8d24c758ebe
Daniela <daniela.fedyakin@gmail.com>
2018-04-06 11:32:29 +0200

    first commit

---
+++ client/reduxstagram.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { render } from 'react-dom'
+// import css
+import css from './styles/style.styl'
+
+// import Components 
+import Main from './components/Main'
+
+render(<Main/>, document.getElementById('root'))
